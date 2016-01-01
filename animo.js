7f6ae345ab963f03b1c82cd32e7b8589520ca4fa
Dimitris Baltas <dbaltas@travelplanet24.com>
2016-01-01 18:06:17 +0200

    layout using twitter bootstrap

--- animo.js
+++ animo.js
@@ -10,19 +10,28 @@ if (Meteor.isServer) {
   });
 
 }
+Router.configure({
+  // the default layout
+  // https://www.codetutorial.io/meteor-routing-and-layout/
+  layoutTemplate: 'layout'
+});
 
     Router.route('/browse', function () {
       this.render('browse');
+      this.layout('layout');
     });
 
     Router.route('/', function () {
       this.render('index');
+      this.layout('layout');
     });
 
     Router.route('/foo', function () {
       this.render('foo');
+      this.layout('layout');
     });
 
     Router.route('/error', function () {
       this.render('error');
+      this.layout('layout');
     });
--- client/templates/browse.js
+++ client/templates/browse.js
@@ -1,6 +1,3 @@
-console.log('hey');
-console.log(Meteor.userId());
-
 Template.browse.rendered = function(){
 	if (!Meteor.userId()) {
 		Router.go('/error');
